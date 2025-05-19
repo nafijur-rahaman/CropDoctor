3a343ae2ac965f304125cf1b9a442abacc79ef1c
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-05-20 00:13:01 +0600

    setup react application

---
+++ frontend/Web Application/crop_doctor/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className="text-3xl text-center mt-10 text-green-500 font-semibold ">CropDoctor</h1>
+
+    </>
+  )
+}
+
+export default App
